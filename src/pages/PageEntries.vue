48a1f1c820250e351a9c0b1ce63b4185bcc91183
veyselustuntas <veyselustuntas@hotmail.com>
2024-12-30 15:28:28 +0300

    feat: auto focus name input & defatult entry form

--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -4,16 +4,16 @@
       <q-list separator bordered>
         <q-item v-for="entry in entries" :key="entry.id">
           <q-item-section
-            :class="useAmountColorClass(entry.amount)"
+            :class="useAmountColorClass(entry.amount!)"
             class="text-weight-bold"
             >{{ entry.name }}</q-item-section
           >
 
           <q-item-section
-            :class="useAmountColorClass(entry.amount)"
+            :class="useAmountColorClass(entry.amount!)"
             class="text-weight-bold"
             side
-            >{{ useCurrencify(entry.amount) }}
+            >{{ useCurrencify(entry.amount!) }}
           </q-item-section>
         </q-item>
       </q-list>
@@ -22,24 +22,21 @@
     <q-footer class="bg-transparent">
       <div class="row q-mb-sm q-px-md q-py-sm shadow-up-3">
         <div class="col text-grey-7 text-h6">Balance:</div>
-        <div
-          class="col text-h6 text-right"
-          :class="useAmountColorClass(balance)"
-        >
+        <div class="col text-h6 text-right" :class="useAmountColorClass(balance)">
           {{ useCurrencify(balance) }}
         </div>
       </div>
 
-      <q-form
-      @submit="addEntry"
-      class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary">
+      <q-form @submit="addEntry" class="row q-px-sm q-pb-sm q-col-gutter-sm bg-primary">
         <div class="col-5">
           <q-input
             placeholder="Name"
             bg-color="white"
             dense
             outlined
-            v-model="addEntryForm.name" />
+            ref="nameRef"
+            v-model="addEntryForm.name"
+          />
         </div>
         <div class="col-5">
           <q-input
@@ -54,12 +51,7 @@
           />
         </div>
         <div class="col-2">
-          <q-btn
-            color="positive"
-            icon="add"
-            round
-            type="submit"
-            />
+          <q-btn color="positive" icon="add" round type="submit" />
         </div>
       </q-form>
     </q-footer>
@@ -70,14 +62,13 @@
 import { useAmountColorClass } from "src/use/useAmountColorClass";
 import { useCurrencify } from "src/use/useCurrencify";
 import { computed, reactive, ref } from "vue";
-import {uid} from "quasar"
+import { uid } from "quasar";
 
-interface IEntry{
-  id:string,
-  name:string,
-  amount:number
+interface IEntry {
+  id: string;
+  name: string;
+  amount: number | null;
 }
-
 
 const entries = ref<IEntry[]>([
   {
@@ -102,25 +93,31 @@ const entries = ref<IEntry[]>([
   },
 ]);
 
+const nameRef = ref<HTMLInputElement | null>(null);
+
 const balance = computed(() => {
-  return entries.value.reduce((acc, {amount}) => {
-    return acc + amount
-  },0);
+  return entries.value.reduce((acc, { amount }) => {
+    return acc + amount!;
+  }, 0);
 });
 
+const addEntryFormDefault = {
+  name: "",
+  amount: null,
+};
+
 const addEntryForm = reactive({
-  name:"",
-  amount:null
-})
+  ...addEntryFormDefault,
+});
+
+const addEntryFormReset = () => {
+  Object.assign(addEntryForm, addEntryFormDefault);
+  nameRef.value!.focus();
+};
 
 const addEntry = () => {
-  const newEntry:IEntry = {
-    id: uid(),
-    name:addEntryForm.name,
-    amount:(Number)(addEntryForm.amount)
-  }
+  const newEntry = Object.assign({}, { id: uid() }, addEntryForm);
   entries.value.push(newEntry);
-}
-
-
+  addEntryFormReset();
+};
 </script>
